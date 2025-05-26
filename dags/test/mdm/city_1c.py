7ae4764030507f2305b846de45024cb78806da05
Tr8ch <rak_0111@mail.ru>
2025-05-26 11:56:51 +0500

    test check

--- dags/test/mdm/city_1c.py
+++ dags/test/mdm/city_1c.py
@@ -11,6 +11,23 @@ from airflow.providers.elasticsearch.hooks.elasticsearch import ElasticsearchPyt
 
 DICTIONARY_NAME = "city"
 INDEX_NAME = f"{DICTIONARY_NAME}_1c"
+ZERO_UUID = "00000000-0000-0000-0000-000000000000"
+
+
+def request_to_1c(host: str, dic_name: str) -> dict:
+    url = f"{host}/send/by_db_name/AstOffice/getbaseinfo/{dic_name}"
+
+    resp = requests.post(url, timeout=30)
+    resp.raise_for_status()
+    return resp.json()
+
+
+def normalize_zero_uuid_fields(item: dict, fields: list[str]) -> dict:
+    """Заменяет ZERO_UUID на пустую строку в указанных полях."""
+    for field in fields:
+        if item.get(field) == ZERO_UUID:
+            item[field] = ""
+    return item
 
 
 def fetch_data_callable(**context) -> None:

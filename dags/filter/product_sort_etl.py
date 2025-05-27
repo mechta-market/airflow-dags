fa2b165dd9eabfc1a6402dba02995a0b3a74b39c
Tr8ch <rak_0111@mail.ru>
2025-05-27 14:41:36 +0500

    fix: load into file while tasks

--- dags/filter/product_sort_etl.py
+++ dags/filter/product_sort_etl.py
@@ -1,3 +1,5 @@
+import os
+import json
 from datetime import datetime
 from helpers.utils import request_to_site_api
 
@@ -7,15 +9,35 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.providers.elasticsearch.hooks.elasticsearch import ElasticsearchPythonHook
 
 INDEX_NAME = "product_v1"
+DATA_FILE_PATH = "/tmp/product_sort_site.json"
+
 
 def fetch_data_callable(**context):
-    response = request_to_site_api(host=Variable.get("site_api_host"), endpoint="v2/airflow/product/sort")
-    context["ti"].xcom_push(key="fetched_data", value=response.get("products"))
+    response = request_to_site_api(
+        host=Variable.get("site_api_host"), endpoint="v2/airflow/product/sort"
+    )
+    with open(DATA_FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump(response, f, ensure_ascii=False)
+
+    print(f"Data saved to {DATA_FILE_PATH}")
+    context["ti"].xcom_push(key="data_file_path", value=DATA_FILE_PATH)
+
 
 def upsert_to_es_callable(**context):
-    items = context["ti"].xcom_pull(key="fetched_data", task_ids="fetch_data_task")
+    file_path = context["ti"].xcom_pull(
+        key="data_file_path", task_ids="fetch_data_task"
+    )
+
+    if not file_path or not os.path.exists(file_path):
+        print("Data file not found.")
+        return
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        items = json.load(f)
+
     if not items:
         return
+
     hosts = ["http://mdm.default:9200"]
     es_hook = ElasticsearchPythonHook(hosts=hosts)
     client = es_hook.get_conn
@@ -28,6 +50,7 @@ def upsert_to_es_callable(**context):
             id=doc_id,
             body={"doc": {"sort": item.get("sort")}, "doc_as_upsert": True},
         )
+
 
 default_args = {
     "owner": "Sultan",

70a4f2936512b9db0450575ecb63486a5d044415
Tr8ch <rak_0111@mail.ru>
2025-05-27 15:30:02 +0500

    fix: check

--- dags/test/mdm/city_1c.py
+++ dags/test/mdm/city_1c.py
@@ -86,7 +86,10 @@ def upsert_city_ids_in_warehouse_callable(**context):
 
     warehouse_city_ids = {}
     for item in items:
-        for warehouse_id in item.get("warehouse_ids"):
+        warehouse_ids = item.get("warehouse_ids")
+        logging.info(f"warehouse_ids: {warehouse_ids}")
+
+        for warehouse_id in warehouse_ids:
             if not warehouse_id:
                 continue
             city_id = item.get("city_id")

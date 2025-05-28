10664c72ebee0f487d253f662bb8f2acb7de4c2c
Olzhas <ol.ilyassov@gmail.com>
2025-05-28 16:34:28 +0500

    fix: cron

--- dags/filter/product_price_etl.py
+++ dags/filter/product_price_etl.py
@@ -565,7 +565,7 @@ with DAG(
     default_args=default_args,
     description='DAG to upload product_price data from Price service to Elasticsearch index',
     start_date=datetime(2025, 5, 22),
-    schedule="*30 * * * *",
+    schedule="30 * * * *",
     catchup=False,
     tags=["nsi", "elasticsearch", "price"],
 ) as dag:
